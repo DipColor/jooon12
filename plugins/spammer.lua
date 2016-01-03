fc7c44c802971a048c4316fbfd29450a77cd1c87
amirfucker <amirhosseintalebi26@yahoo.com>
2016-01-03 22:31:30 +0330

    Create spammer.lua

---
+++ plugins/spammer.lua
@@ -0,0 +1,23 @@
+do
+function run(msg, matches)
+
+
+  local tex = matches[1]
+  local sps = matches[2]
+for i=1, tex, 1 do
+
+sp = '\n'..sps..'\n'..sp
+i = i + 1
+end
+return sp
+
+
+         end
+return {
+    patterns = {
+      "^[/!][Ss]pam (.*) (.*)$"
+    },
+    run = run,
+}
+
+end
