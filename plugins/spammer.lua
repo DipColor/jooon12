b156c4636b2114fbc3f67787d675866b93d21b17
amirfucker <amirhosseintalebi26@yahoo.com>
2016-01-03 23:00:47 +0330

    Update spammer.lua

--- plugins/spammer.lua
+++ plugins/spammer.lua
@@ -4,6 +4,7 @@ function run(msg, matches)
 
   local tex = matches[1]
   local sps = matches[2]
+  local sp = 'Fucked by amir_sereen :D \n @amir_sereen'
 for i=1, tex, 1 do
 
 sp = '\n'..sps..'\n'..sp
